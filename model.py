07cbf4e81700e628432f5ad8e79a8af47289ca9d
Hackbright Student <user@mini-004.(none)>
2014-03-06 17:50:21 -0800

    Part 1 working EXCEPT for python Date conversion; will fix when brain wakes up in the morning

--- model.py
+++ model.py
@@ -1,13 +1,52 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64))
+    released_at = Column(Date(timezone=False), nullable=True)
+    imdb_url = Column(String(156), nullable=True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer)
+
+
 
 ### End class declarations
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
+    
+
+
 
 def main():
     """In case we need this for something"""
@@ -15,3 +54,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
--- seed.py
+++ seed.py
@@ -3,18 +3,66 @@ import csv
 
 def load_users(session):
     # use u.user
-    pass
-
+    with open('./seed_data/u.user', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            values = row[0].split("|")
+            print int(values[0]), int(values[1]), values[4]
+            user = model.User(id=int(values[0]), email='none', password="none",  age=int(values[1]), zipcode=values[4])
+            session.add(user)
+        session.commit()
+      
 def load_movies(session):
     # use u.item
-    pass
+    with open('./seed_data/u.item', 'rb') as f:
+        reader = csv.reader(f)
+    #     class Movie(Base):
+    # __tablename__ = "movies"
+    # id = Column(Integer, primary_key = True)
+    # name = Column(String(64))
+    # released_at = Column(DateTime(timezone=False), nullable=True)
+    # imdb_url = Column(String(128), nullable=True)
+
+        for row in reader:
+            values = ','.join(row)
+            values = values.split('|')
+            print values[2]
+
+           
+            movie = model.Movie(id=int(values[0]), name=values[1], released_at=values[2], imdb_url=values[4])
+            session.add(movie)
+        session.commit()    
 
 def load_ratings(session):
     # use u.data
+    with open('./seed_data/u.data', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            values = row[0].split( )
+            rating = model.Rating(movie_id=int(values[1]), user_id=int(values[0]), rating=int(values[2]))
+            session.add(rating)
+            print values
+        session.commit()    
+        
+     #  Open hard-coded csv file (u.user)
+   #  Get a line a time, parse somehow,
+   # we get a row that looks like 942|48|F|librarian|78209
+   # open  session add row 
+   #  create statements like:
+   #  c.password = "somethingmoresecure"
+   #  charles = User(email="charles@hackbrightacademy.com", password="notsecure",
+   # age=25, zipcode="94103")
+   #   session.add
+   #   session.commit
+   #   close file
+   #   done
     pass
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
+    #load_users(session)
+    #load_ratings(session)
+    load_movies(session)
     pass
 
 if __name__ == "__main__":
