68b8519c0e20b95cd8bf6a6917d853a07a7a5ba5
Hackbright Student <user@mini-004.(none)>
2014-03-11 17:52:17 -0700

    Finished other than getting rating to post.

--- judgement.py
+++ judgement.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, request, flash, session
 import model
+import hashlib
 
 app = Flask(__name__)
+#Passed through the session with the user so that we know it is the same user
 app.secret_key = 'some_secret'
+salt = " 10 Pink Unicorns = the best"
 
 @app.route("/")
 def index():
@@ -16,32 +19,45 @@ def user_login_form():
 
 @app.route("/user/login", methods=['POST'])
 def user_login():
+    global salt
      # Get login data for user from login-user form
     username = request.form.get("username")
-    password = request.form.get("password")
+    password = hashlib.sha1(request.form.get("password")+salt).hexdigest()
 
     user_list = model.session.query(model.User).filter_by(email=username, password=password).all()
-    if len(user_list) == 0:
-        flash("User does not exist.")
-        return render_template("login_user.html")
-    else:
-        #Note we are ignoring the case in which there are multiple users with the credentials
-        flash("User authenticated.")
+    if user_list:
         session['username'] = username
-
-        print session, session['username']
+        flash("User, " + session["username"] + ", authenticated.")
         id = user_list[0].id
-        ratings = model.session.query(model.Rating).filter_by(user_id=id)    
-    return render_template("view_user.html", user=user_list[0],ratings=ratings)    
-    
+        ratings = model.session.query(model.Rating).filter_by(user_id=id)
+        return render_template("view_user.html", user=user_list[0],ratings=ratings)
+    #login error
+    else:
+        flash("User not authenticated.")
+        return render_template("login_user.html")              
+
+
 @app.route("/search/movie", methods=['POST'])
 def movie_search():
+    #create variable to filter in like
     name = request.form.get("name")
-    print name
-    movies = model.session.query(model.Movie).filter_by(name=name).all()
-    for m in movies:
-         print movies[m].name
-    return "blah blah blah"
+    #query for the movie typed in the form
+    movies = model.session.query(model.Movie).filter(model.Movie.name.like("%"+name+"%")).all()
+    #set up a dictionary to show ratings for each movie - this will pass into the template
+    ratings_dict = {}
+    #get ratings for the movies
+    for movie in movies:
+        if movie.id not in ratings_dict:
+            ratings_dict[movie.id] = []
+        #gets the ratings for each movie returned
+        ratings = model.session.query(model.Rating).filter_by(movie_id = movie.id).all()
+        for rating in ratings:
+            #ratings_dict[movie.id].append(rating.rating)
+            ratings_dict[movie.id].append({"user_id": rating.user_id, "rating": rating.rating })
+    if not movies:
+        flash("No movies were found containing this name.")
+        return render_template("movie_search.html")
+    return render_template("movie_results.html", movies = movies, ratings = ratings_dict)
 
 @app.route("/search/movie", methods=['GET'])
 def movie_search_form():
@@ -55,6 +71,15 @@ def view_user(id):
     ratings = model.session.query(model.Rating).filter_by(user_id=id)    
     return render_template("view_user.html", user=user, ratings=ratings)
 
+# Display a single movie record
+@app.route("/movie/<id>", methods=['GET'])
+def view_movie(id):
+    movie = model.session.query(model.Movie).get(id)
+    ratings = model.session.query(model.Rating).filter_by(user_id=id)
+
+    # Display the movie record and update form
+    return render_template("rate_movie.html", movie=movie, ratings=ratings)
+
 # Create a new user
 @app.route("/user/new", methods=['GET'])
 def new_user_form():
@@ -63,17 +88,24 @@ def new_user_form():
 
 @app.route("/user/new", methods=['POST'])
 def new_user():
+    global salt
     # Get data for user from request.form
     username = request.form.get("username")
-    password = request.form.get("password")
+    password = hashlib.sha1(request.form.get("password")+salt).hexdigest()
     age = request.form.get("age")
     zipcode = request.form.get("zipcode")
+    user_exist = model.session.query(model.User).filter_by(email=username).all()
+    #if user already exists, return to new user form
+    if user_exist:
+        flash("User already exists.")
+        return render_template("new_user_form.html")
     # Create the user object
     user = model.User(email=username, password=password, age=age, zipcode=zipcode)
     # commit to database
     model.session.add(user)
     model.session.commit()
-    return render_template("new_user_form.html")
+    session['username'] = username
+    return render_template("view_user.html", user=user)
 
     
 # Update an existing user
